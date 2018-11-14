9fc3c372454828eb4e14d10c852a9aa843cb5ed9
Steven De Cock <steven.decock@realdolmen.com>
2018-11-14 16:59:00 +0100

    solution ex 17

--- exercise17-ajax/scripts/scripts.js
+++ exercise17-ajax/scripts/scripts.js
@@ -1,5 +1,13 @@
 $(function() {
   
   $('#no-script').remove();
-
+  $.getJSON("https://mysafeinfo.com/api/data?list=englishmonarchs&format=json",function(data){
+    $.each(data,function(){
+      var art = $('<article></article>');
+      art.append("<h1>"+this.nm+"</h1>");
+      art.append("<p>"+this.hse+"</p>");
+      art.append("<span>"+this.yrs+"</span>");
+      $("#main").append(art);
+    })
+  })
 });
